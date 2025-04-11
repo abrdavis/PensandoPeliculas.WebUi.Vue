6179bb9984b716614fe50f99a2ec996007e7d667
Adam Davis <ravel@development>
2025-04-11 15:49:41 -0400

    Logging for env issues.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@
     import { reviewService } from '@/services/reviewService';
     import { ref } from 'vue'
 
-  
+  console.log(import.meta.env.VITE_API_URL);
 
     const recentReviews = ref([]);
     const recentReleaseReviews = ref([]);
